1d243603ab58d979ca44df140755881f5273eb6b
zhang.yufeng <420866750@qq.com>
2016-12-28 11:14:25 +0800

    globalstyles

--- global/gulpfile.js
+++ global/gulpfile.js
@@ -6,19 +6,30 @@ watch = require('gulp-watch'),
 px2rem = require('gulp-px2rem'),
 cssmin = require('gulp-minify-css');
 
-gulp.task('browser-sync', ['sass'], function(){
+gulp.task('browser-sync', ['sass-pc', 'sass-wap'], function(){
   browserSync.init({
     server:{
     	baseDir:'./demo'
     }
   });
   
-  gulp.watch('./src/*.scss', [ 'sass' ]);
+  gulp.watch('./src/global.pc.scss', [ 'sass-pc' ]);
+  gulp.watch('./src/global.wap.scss', [ 'sass-wap' ]);
 });
 
-gulp.task('sass', function(){
-	return gulp.src('./src/*.scss')
+gulp.task('sass-pc', function(){
+  return gulp.src('./src/global.pc.scss')
       .pipe( sass() )
+      .pipe( cssmin() )
+      .pipe( gulp.dest( './demo' ) )
+      .pipe( browserSync.stream() );
+});
+
+gulp.task('sass-wap', function(){
+	return gulp.src('./src/global.wap.scss')
+      .pipe( sass() )
+      .pipe( px2rem({ replace: true, rootValue: 64 }, { map: true }) )
+      .pipe( cssmin() )
     	.pipe( gulp.dest( './demo' ) )
       .pipe( browserSync.stream() );
 });
